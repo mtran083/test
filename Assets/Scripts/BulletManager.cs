20fb4a6e1e4a434dfb1d56f50b68b7a483009661
Michael Tran <mtran083@gmail.com>
2014-07-24 21:11:30 -0400

    Finished scrolling and shooting

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    // Public field
+    public int pool_size = 50;
+
+    // Private fields
+    private int b_idx;
+    private Transform[] b_list;
+
+    // Static singleton property
+    public static BulletManager Instance { get; private set; }
+
+    void Awake()
+    {
+        // First we check if there are any other instances conflicting
+        if (Instance != null && Instance != this)
+        {
+            // If that is the case, we destroy other instances
+            Destroy(gameObject);
+        }
+
+        // Here we save our singleton instance
+        Instance = this;
+
+        // Furthermore we make sure that we don't destroy between scenes (this is optional)
+        DontDestroyOnLoad(gameObject);
+
+        b_idx = 0;
+        b_list = new Transform[pool_size];
+    }
+
+    // Instance method, this method can be accesed through the singleton instance
+    public void add_bullet(Transform t)
+    {
+
+        if (b_list[b_idx] != null)
+        {
+            Destroy(b_list[b_idx].gameObject);
+            //Debug.Log("Deleting Bullet at " + b_idx);
+            b_list[b_idx] = null;
+        }
+
+        b_list[b_idx] = t;
+        b_idx++;
+
+        if (b_idx >= pool_size) b_idx = 0;
+
+        return;
+    }
+
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -24,6 +24,46 @@ public class PlayerScript : MonoBehaviour
           speed.x * inputX,
           speed.y * inputY);
 
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+
+        // 6 - Make sure we are not outside the camera bounds
+        var dist = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z
+        );
+
     }
 
     void FixedUpdate()
@@ -31,4 +71,28 @@ public class PlayerScript : MonoBehaviour
         // 5 - Move the game object
         rigidbody2D.velocity = movement;
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool damagePlayer = false;
+
+        // Collision with enemy
+        EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+        if (enemy != null)
+        {
+            // Kill the enemy
+            HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+            if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+
+            damagePlayer = true;
+        }
+
+        // Damage the player
+        if (damagePlayer)
+        {
+            HealthScript playerHealth = this.GetComponent<HealthScript>();
+            if (playerHealth != null) playerHealth.Damage(1);
+        }
+    }
+
 }
